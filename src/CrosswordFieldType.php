5c2f9460b5878c0fc9041ca286db6cddbfb45500
Denis Efremov <efremov.a.denis@gmail.com>
2019-01-09 11:34:34 +0300

    Initial commit

---
+++ src/CrosswordFieldType.php
@@ -0,0 +1,15 @@
+<?php namespace Defr\CrosswordFieldType;
+
+use Anomaly\Streams\Platform\Addon\FieldType\FieldType;
+
+class CrosswordFieldType extends FieldType
+{
+
+    /**
+     * The field input view.
+     *
+     * @var string
+     */
+    protected $inputView = 'defr.field_type.crossword::input';
+
+}
---
+++ tests/Feature/CrosswordFieldTypeTest.php
@@ -0,0 +1,10 @@
+<?php namespace Defr\CrosswordFieldType\Test\Feature;
+
+class CrosswordFieldTypeTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
